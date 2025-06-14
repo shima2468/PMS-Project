a015595b1e2427029f72398a074391294506b1d7
shimaa2468 <shamazourob@gmail.com>
2025-06-14 04:16:39 -0700

    Initial project setup and placeholder structure

---
+++ src/Modules/Shared/Components/AuthenticationLayout/AuthenticationLayout.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AuthenticationLayout = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default AuthenticationLayout;
---
+++ src/Modules/Tasks/Components/TasksData/TasksData.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TasksData = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default TasksData;
